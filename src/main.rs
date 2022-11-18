9c6ee08768b5765f108cc9650878d7c2fced807a
Ben Clarke <ben@arrayofstars.com>
2022-11-17 20:48:41 -0500

    Logic implementation

--- src/main.rs
+++ src/main.rs
@@ -1,118 +1,77 @@
-use rand::Rng;
-use std::cmp::Ordering;
+use cards::{CardType, Deck, Player};
 
-#[derive(Debug, Eq, PartialEq, Ord, PartialOrd)]
-enum Suit {
-    Spade,
-    Heart,
-    Club,
-    Diamond,
-}
+use std::io;
 
-#[derive(Debug, Eq, PartialEq, Ord, PartialOrd)]
-enum CardType {
-    Number(u8),
-    Jack,
-    Queen,
-    King,
-    Ace,
-}
-
-#[derive(Debug, Eq, PartialEq, PartialOrd)]
-struct Card<'a> {
-    suit: &'a Suit,
-    card_type: &'a CardType,
-}
-
-impl<'a> Card<'a> {
-    fn new(suit: &'a Suit, card_type: &'a CardType) -> Card<'a> {
-        Card::<'a> { suit, card_type }
+fn read_cmd() -> Option<String> {
+    let mut buf = String::new();
+    match io::stdin().read_line(&mut buf) {
+        Ok(_) => Some(buf),
+        Err(_) => None,
     }
 }
 
-impl<'a> Ord for Card<'a> {
-    fn cmp(&self, other: &Self) -> Ordering {
-        if self.suit > other.suit {
-            return Ordering::Greater;
-        }
-
-        if self.suit < other.suit {
-            return Ordering::Less;
-        }
-
-        if self.card_type > other.card_type {
-            return Ordering::Greater;
-        }
-
-        if self.card_type < other.card_type {
-            return Ordering::Less;
-        }
-
-        Ordering::Equal
-    }
-}
-
-#[derive(Debug)]
-struct Deck<'a> {
-    cards: Vec<Card<'a>>,
-}
-
-impl<'a> Deck<'a> {
-    fn new() -> Deck<'a> {
-        let suits = vec![&Suit::Spade, &Suit::Heart, &Suit::Club, &Suit::Diamond];
-
-        let values = (0..13)
-            .map(|x| match x {
-                0 => &CardType::Ace,
-                1 => &CardType::Jack,
-                2 => &CardType::Number(2),
-                3 => &CardType::Number(3),
-                4 => &CardType::Number(4),
-                5 => &CardType::Number(5),
-                6 => &CardType::Number(6),
-                7 => &CardType::Number(7),
-                8 => &CardType::Number(8),
-                9 => &CardType::Number(9),
-                10 => &CardType::Number(10),
-                11 => &CardType::Queen,
-                12 => &CardType::King,
-                _ => panic!(),
-            })
-            .collect::<Vec<&CardType>>();
-
-        let cards = (0..52)
-            .map(|value| {
-                let suit = suits[value % 4];
-                let card_type = values[value % 13];
-
-                Card::new(suit, card_type)
-            })
-            .collect::<Vec<Card>>();
-
-        let mut cards = cards;
-        cards.sort();
-        let cards = cards;
-
-        Deck::<'a> { cards }
-    }
-
-    fn shuffle(&mut self) {
-        let cards = &mut self.cards;
-        let mut rng = rand::thread_rng();
-
-        let len = cards.len() - 1;
-        for i in 0..len {
-            let index = len - i;
-            let random = rng.gen_range(0..index);
-
-            cards.swap(index, random);
-        }
+fn guess_card_type(input: char) -> Option<CardType> {
+    match input {
+        'k' => Some(CardType::King),
+        'q' => Some(CardType::Queen),
+        'j' => Some(CardType::Jack),
+        'a' => Some(CardType::Ace),
+        '2' => Some(CardType::Number(2)),
+        '3' => Some(CardType::Number(3)),
+        '4' => Some(CardType::Number(4)),
+        '5' => Some(CardType::Number(5)),
+        '6' => Some(CardType::Number(6)),
+        '7' => Some(CardType::Number(7)),
+        '8' => Some(CardType::Number(8)),
+        '9' => Some(CardType::Number(9)),
+        '1' => Some(CardType::Number(10)),
+        _ => None,
     }
 }
 
 fn main() {
     let mut deck = Deck::new();
-    deck.shuffle();
+    let mut player1 = Player::new();
+    let mut player2 = Player::new();
 
-    println!("{:?}", deck);
+    let mut players = vec![&mut player1, &mut player2];
+
+    deck.shuffle();
+    deck.deal(7, &mut players);
+
+    let game_over = false;
+
+    while !game_over {
+        {
+            let player1 = &mut player1;
+            let mut pairs = player1.hand.pairs();
+            println!("You burn {}", pairs);
+
+            player1.burn_pile.give_deck(&mut pairs);
+
+            println!("PLAYER HAND - {}", player1.hand);
+            println!("Which card will you ask for?");
+        }
+
+        if let Some(cmd) = read_cmd() {
+            let input = cmd.chars().next().unwrap();
+            if let Some(guess) = guess_card_type(input) {
+                println!("You ask for a {}", guess);
+
+                let next_card = match player2.hand.find_type(guess) {
+                    Some(card) => {
+                        println!("You get the {}!", card);
+                        card
+                    }
+                    None => {
+                        let next = deck.take_card();
+                        println!("GO FISH! You take the {} from the deck", next);
+                        next
+                    }
+                };
+
+                player1.hand.give_card(next_card);
+            }
+        }
+    }
 }
